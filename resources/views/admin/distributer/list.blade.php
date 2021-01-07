2da05b657da376832359f4fd888a497f5ae88714
kankuro12 <cms111000111@gmail.com>
2021-01-07 15:50:04 +0545

    null check for distributor

--- resources/views/admin/distributer/list.blade.php
+++ resources/views/admin/distributer/list.blade.php
@@ -1,16 +1,18 @@
 @foreach($distributer as $user)
-<tr id="distributer-{{$user->id}}" data-name="{{ $user->name }}" class="searchable">
-    <td>{{ $user->distributer()->id }}</td>
-    <td>{{ $user->name }}</td>
-    <td>{{ $user->phone }}</td>
-    <td>{{ $user->address }}</td>
-    {{-- <td>{{ $user->distributer()->rate }}</td>
-    <td>{{ $user->distributer()->amount }}</td> --}}
-    <td>
-        <button type="button" data-distributer="{{$user->toJson()}}" data-rate="{{ $user->distributer()->rate }}" data-amount="{{ $user->distributer()->amount }}" class="btn btn-primary btn-sm" onclick="initEdit(this);">Edit</button>
-        |
-        <a href="{{ route('distributer.detail',$user->id) }}" class="btn btn-primary btn-sm">View</a>
-        |
-        <button class="btn btn-danger btn-sm" onclick="removeData({{$user->id}});">Delete</button></td>
-</tr>
+@if ($user->distributer()!=null)
+    <tr id="distributer-{{$user->id}}" data-name="{{ $user->name }}" class="searchable">
+        <td>{{ $user->distributer()->id }}</td>
+        <td>{{ $user->name }}</td>
+        <td>{{ $user->phone }}</td>
+        <td>{{ $user->address }}</td>
+        {{-- <td>{{ $user->distributer()->rate }}</td>
+        <td>{{ $user->distributer()->amount }}</td> --}}
+        <td>
+            <button type="button" data-distributer="{{$user->toJson()}}" data-rate="{{ $user->distributer()->rate }}" data-amount="{{ $user->distributer()->amount }}" class="btn btn-primary btn-sm" onclick="initEdit(this);">Edit</button>
+            |
+            <a href="{{ route('distributer.detail',$user->id) }}" class="btn btn-primary btn-sm">View</a>
+            |
+            <button class="btn btn-danger btn-sm" onclick="removeData({{$user->id}});">Delete</button></td>
+    </tr>
+@endif
 @endforeach
